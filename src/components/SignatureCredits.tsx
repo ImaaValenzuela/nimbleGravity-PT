1db4a7c68ad3642bcb220be875940a77408d3b90
Imanol Valenzuela <imanolvalenzuela03@gmail.com>
2026-02-21 13:42:09 -0300

    hotfix: delete import in signature

--- src/components/SignatureCredits.tsx
+++ src/components/SignatureCredits.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { Github, Linkedin } from 'lucide-react';
+import { Github, Linkedin } from "lucide-react";
 
 export const SignatureCredits = () => {
-    return (
-        <div className="signature-credits">
-            <span className="signature-text">Crafted by Imanol</span>
-            <div className="signature-links">
-                <a href="https://github.com/ImaaValenzuela" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
-                    <Github className="lucide-icon" strokeWidth={1.5} />
-                </a>
-                <a href="https://www.linkedin.com/in/imanol-valenzuela/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-                    <Linkedin className="lucide-icon" strokeWidth={1.5} />
-                </a>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="signature-credits">
+      <span className="signature-text">Crafted by Imanol</span>
+      <div className="signature-links">
+        <a
+          href="https://github.com/ImaaValenzuela"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <Github className="lucide-icon" strokeWidth={1.5} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/imanol-valenzuela/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <Linkedin className="lucide-icon" strokeWidth={1.5} />
+        </a>
+      </div>
+    </div>
+  );
+};
